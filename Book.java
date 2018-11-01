522ab5080af27f26725cdae769db34d517acc0b1
Ziyu <16078718+NeilWangziyu@users.noreply.github.com>
2018-11-01 21:39:23 +0800

    2018-11-1

---
+++ Book.java
@@ -0,0 +1,27 @@
+public class Book {
+    private int id;
+    private String name;
+    private String category;
+    public String getName(){
+        int id=0;
+        setName("Java");
+        return id+this.name;
+    }
+
+    private void setName(String name){
+        this.name=name;
+    }
+    public Book getBook(){
+        return this;
+    }
+
+
+    public static void main(String[] args){
+        System.out.println("out");
+
+    }
+
+}
+
+
+
