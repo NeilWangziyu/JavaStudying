e41ff827361ac1fb382d3e850d2fb0175679f757
Ziyu <16078718+NeilWangziyu@users.noreply.github.com>
2018-11-01 21:39:08 +0800

    2018-11-1

---
+++ Books.java
@@ -0,0 +1,23 @@
+public class Books {
+    private String title;
+    private String author;
+    private double price;
+    public Books(String title, String author, double price){
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+}
